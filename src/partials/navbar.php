2d8f42bbf6b5a878c91b353b64cffadeee0b00b3
Mouschi7 <johnbaarde07@gmail.com>
2025-08-31 02:08:15 +0800

    Initial commit - setup project

---
+++ src/partials/navbar.php
@@ -0,0 +1,23 @@
+
+<header>
+            <nav class="navbar">
+                  <div class="nav-container">
+                        <div class="nav-left">
+                              <a href="/website/src/home.php">Home</a>
+                              <a href="/website/src/about.php">About Us</a>        
+                              <a href="/website/src/menu.php">Menu</a>       
+                              <a href="/website/src/gallery.php">Gallery</a>
+                        </div>
+                        <div class="nav-logo">
+                              <a href="/website/src/home.php">
+                                    <h1 class="logo1">CAFE</h1>
+                                    <h1 class="logo2">CHILL</h1>
+                              </a>
+                        </div>
+                        <div class="nav-right">
+                              <a href="/website/src/cart.php">Cart</a>
+                              <a href="/website/src/auth.php">Sign-Up</a>
+                        </div>
+                  </div>
+            </nav>
+      </header>
