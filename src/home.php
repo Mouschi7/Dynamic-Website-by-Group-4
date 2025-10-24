b058254fc0960ede0c3c65b67d572e1bbaf99bba
Mouschi7 <johnbaarde07@gmail.com>
2025-10-25 03:03:49 +0800

    commit

--- src/home.php
+++ src/home.php
@@ -27,18 +27,36 @@
                         <a class="cta-button" href="/website/src/menu.php">View Menu</a>
                   </div>
             </section>
+
+            <section class="section why-section">
+            <div class="why-container">
+                  <div class="why-content-wrapper">
+                        <div class="why-text">
+                              <h2>Why Kusina ni Kapetan?</h2>
+                              <p class="why-description">
+                                    Food service is more than just preparing and serving meals- it's about creating memorable experiences for every customer. It continues quality ingredients, skilled preparation, and a welcoming atmosphere to ensure that many dish things satisfaction and joy. A great food service focuses on freshness, flavor, and presentation to delight the senses.
+                              </p>
+                              <button class="why-button" href="/src/menu.php">hello</button>
+                        </div>
+                        <div class="why-image">
+                              <img src="assets/images/sample3.jpg" alt="Kusina ni Kapetan Food" />
+                        </div>
+                  </div>
+            </div>
+            </section>
+
+            <section class="section best-seller">
+                  <div class="best-seller-container">
+                        <div class="best-seller-content">
+                              <h2>Why Kusina ni Kapetan?</h2>
+                              <p class="why-description">
+                                    Food service is more than just preparing and serving meals- it's about creating memorable experiences for every customer. It continues quality ingredients, skilled preparation, and a welcoming atmosphere to ensure that many dish things satisfaction and joy. A great food service focuses on freshness, flavor, and presentation to delight the senses.
+                              </p>
+                        </div>
+                  </div>
+            </section>
+            
             
       </main>
-      
 </body>
 </html>
-
-
-
-
-
-<!--<video autoplay muted loop playsinline class="hero-bg">
-                        <source src="/website/videos/bgedit.mp4">
-                        Your browser does not support the video tag.
-                  </video>
-                  -->
